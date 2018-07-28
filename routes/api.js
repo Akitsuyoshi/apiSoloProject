98bef1d4eebc740dd1452ec4abeb9a3644a67bd1
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-07-28 20:40:36 +0900

    Add api to get all yuruChara

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../db/knex');
 
 /* GET api listing. */
 router.get('/', function(req, res, next) {
-  res.send('this is for api');
+  return knex('localchara')
+    .select()
+    .then(charas => {
+      res.json({ charas });
+    })
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message);
+    });
 });
 
 module.exports = router;
