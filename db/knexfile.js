761390090c3042c3f6717c13cb4905e68ee86517
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-07-28 15:47:08 +0900

    Add configuration file for DB

--- db/knexfile.js
+++ db/knexfile.js
@@ -8,26 +8,11 @@ module.exports = {
       database: "yuruChara"
     },
     migrations: {
+      directory: "./migrations",
       tableName: "knex_migrations"
     },
     port: 5432,
     acquireConnectionTimeout: 10000,
     debug: true
-  },
-
-  production: {
-    client: "pg",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "migrations"
-    }
   }
 };
